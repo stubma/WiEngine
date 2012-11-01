69a205ab995e9578a98221613c44144aef4f7fa1
luma <stubma@gmail.com>
2012-11-01 14:23:57 +0800

    transparent drawing primitives demo works

--- samples/WiEngineDemos_native/jni/Demos/Others/OtherDemos.cpp
+++ samples/WiEngineDemos_native/jni/Demos/Others/OtherDemos.cpp
@@ -1475,94 +1475,12 @@ namespace Other {
 	//////////////////////////////////////////////////////////////////////////////////////////
 
 #if ANDROID
-	class wyTransparentDrawPrimitivesTestLayer : public wyLayer {
+	class wyTransparentDrawPrimitivesTestLayer : public wyDrawPrimitivesTestLayer {
 	public:
-		wyTransparentDrawPrimitivesTestLayer(){
-			wyRotateBy* rotate = wyRotateBy::make(4, -360);
-			runAction(rotate);
+		wyTransparentDrawPrimitivesTestLayer() {
 		}
 		
-		virtual ~wyTransparentDrawPrimitivesTestLayer(){
-		}
-		
-		virtual void draw() {
-			// TODO gles2
-//			// 画简单线段
-//			// 默认参数: 宽度 1, 颜色 RGB(255,255,255,255)
-//			// Anti-Aliased
-//			glEnable(GL_LINE_SMOOTH);
-//
-//			wyDrawLine(0, 0, wyDevice::winWidth, wyDevice::winHeight);
-//
-//			// 关掉反锯齿
-//			glDisable(GL_LINE_SMOOTH);
-//			// 设置线段宽度, 宽度大于1时，无法使用GL_LINE_SMOOTH。
-//			glLineWidth(5.0f);
-//			// 设置颜色
-//			glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-//			wyDrawLine(0, wyDevice::winHeight, wyDevice::winWidth, 0);
-//
-//			// 记住：OpenGL是一个状态机.如果你不再次设置画图参数，当前属性不会改变。
-//
-//			// 在中心画一个大点（半径为64)
-//			glPointSize(64);
-//			glColor4f(0.0f, 0.0f, 1.0f, 0.5f);
-//			wyDrawPoint(wyDevice::winWidth / 2, wyDevice::winHeight / 2);
-//
-//			// 画四个小点（半径为4）
-//			float points[] = {
-//				60, 60, 70, 70, 60, 70, 70, 60
-//			};
-//			glPointSize(4);
-//			glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
-//			wyDrawPoints(points, sizeof(points) / sizeof(float));
-//
-//			// 画一个绿色圆圈，圆圈被分成10段
-//			glLineWidth(16);
-//			glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-//			wyDrawCircle(wyDevice::winWidth / 2, wyDevice::winHeight / 2, 100, 0, 10, false);
-//
-//			// 画一个青色圆圈，圆圈被分成50段。并且带一条中心到圆周的直线。
-//			// draw a green circle with 50 segments with line to center
-//			glLineWidth(2);
-//			glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
-//			wyDrawCircle(wyDevice::winWidth / 2, wyDevice::winHeight / 2, 50, 90, 50, true);
-//
-//			// 画一个黄色的多边形
-//			glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
-//			glLineWidth(10);
-//			float vertices[] = {
-//				0, 0, 50, 50, 100, 50, 100, 100, 50, 100
-//			};
-//			wyDrawPoly(vertices, sizeof(vertices) / sizeof(float), false);
-//
-//			// 画闭合多边形
-//			glColor4f(1.0f, 0.0f, 1.0f, 1.0f);
-//			glLineWidth(2);
-//			float vertices2[] = {
-//				30, 130, 30, 230, 50, 200
-//			};
-//			wyDrawPoly(vertices2, sizeof(vertices2) / sizeof(float), true);
-//
-//			// 画"二次贝赛尔曲线"
-//			wyBezierConfig c = wybcQuad(0, wyDevice::winHeight, wyDevice::winWidth / 2, wyDevice::winHeight / 2, wyDevice::winWidth, wyDevice::winHeight);
-//			wyDrawBezier(c, 50);
-//
-//			// 画"三次贝塞尔曲线"
-//			c = wybcCubic(wyDevice::winWidth / 2,
-//						  wyDevice::winHeight / 2,
-//						  wyDevice::winWidth / 2 + 30,
-//						  wyDevice::winHeight / 2 + 50,
-//						  wyDevice::winWidth / 2 + 60,
-//						  wyDevice::winHeight / 2 - 50,
-//						  wyDevice::winWidth,
-//						  wyDevice::winHeight / 2);
-//			wyDrawBezier(c, 100);
-//
-//			// 重置
-//			glLineWidth(1);
-//			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-//			glPointSize(1);
+		virtual ~wyTransparentDrawPrimitivesTestLayer() {
 		}
 	};
 #endif
