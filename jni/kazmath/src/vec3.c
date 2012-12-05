2a5e62b760d1d0b11d00a2be6f86eca5e9ea5adb
luma <stubma@gmail.com>
2012-12-05 10:03:26 +0800

    normalize a zero vector should be always zero

--- jni/kazmath/src/vec3.c
+++ jni/kazmath/src/vec3.c
@@ -70,7 +70,10 @@ kmScalar kmVec3LengthSq(const kmVec3* pIn)
   */
 kmVec3* kmVec3Normalize(kmVec3* pOut, const kmVec3* pIn)
 {
-	kmScalar l = 1.0f / kmVec3Length(pIn);
+    kmScalar l = kmVec3Length(pIn);
+    if(l != 0) {
+        l = 1.0f / l;
+    }
 
 	kmVec3 v;
 	v.x = pIn->x * l;
