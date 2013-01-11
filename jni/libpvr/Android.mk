750903c27b7731de2c61be788d8c2c1815163c6d
luma <stubma@gmail.com>
2013-01-11 15:03:16 +0800

    add more pvr code, update macosx project, however, macosx won't support pvr (cherry picked from commit d4fe8673de4266f84aabf5960682bf130250d3bd)
    Conflicts:
    	MacOSX/WiEngine.xcodeproj/project.pbxproj

--- jni/libpvr/Android.mk
+++ jni/libpvr/Android.mk
@@ -4,9 +4,11 @@
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pvr
-LOCAL_LDLIBS := -llog
-LOCAL_CFLAGS += -fvisibility=hidden
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libpvr
+LOCAL_LDLIBS := -llog -lGLESv2
+LOCAL_CFLAGS += -fvisibility=hidden -DEGL_NOT_PRESENT
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libpvr \
+	$(LOCAL_PATH)/libpvr/OGLES2
+LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES)
 LOCAL_SRC_FILES := $(call all-cpp-files-under,libpvr)
 	
 include $(BUILD_STATIC_LIBRARY)
