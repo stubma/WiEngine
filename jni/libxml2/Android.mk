433ff181041750e1ec3b95fb1592381da61bf777
luma <stubma@gmail.com>
2013-07-30 10:58:10 +0800

    compiled in ndk r9

--- jni/libxml2/Android.mk
+++ jni/libxml2/Android.mk
@@ -7,5 +7,6 @@ LOCAL_MODULE := xml2
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/libxml2/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libxml2/include
 LOCAL_SRC_FILES := $(call all-c-files-under,libxml2)
+LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 	
 include $(BUILD_STATIC_LIBRARY)
